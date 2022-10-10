b1f566e97f964e36e28836d3e9c2a77980f86df6
Luis Roberto <luis.r.s14@hotmail.com>
2022-10-10 20:57:57 -0300

    criado a tela inicial e começando tela do form de login

---
+++ app/src/main/java/com/example/topsegurosbrasilmobile/Login2.java
@@ -0,0 +1,36 @@
+package com.example.topsegurosbrasilmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Login2 extends AppCompatActivity {
+
+    private Button me_cadastrar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+
+        getSupportActionBar().hide();
+        IniciarComponentes();
+
+        me_cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Login2.this, FormCadastro.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void IniciarComponentes(){
+        me_cadastrar = findViewById(R.id.me_cadastrar);
+    }
+}
